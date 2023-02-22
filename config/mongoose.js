a56c761fa10cd09e2550158701aba41d71f854d9
Harsh Gupta <harsh11081999@gmail.com>
2023-02-22 16:48:35 +0530

    mongoDb atlas connected

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose= require('mongoose');
-const localdb = `mongodb://localhost/issueTracker`;
-const dburl = process.env.DB_URL || localdb ;
-
-mongoose.connect(dburl);
-
+//  const localdb = `mongodb://localhost/issueTracker`;
+//  const dburl = process.env.DB_uri || localdb ;
+const Db_uri ='mongodb+srv://harsh1108:harsh1108@cluster0.zvz1y6i.mongodb.net/issue-tracker?retryWrites=true&w=majority';
+// mongoose.connect(`${process.env.Db_uri}`);
+mongoose.connect(Db_uri);
 const db = mongoose.connection;
 // error
 db.on("error", console.error.bind(console, "error connecting to db"));
